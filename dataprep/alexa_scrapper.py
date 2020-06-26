51b849bde978cd634162b511abdb710d046c86bd
PacoPacov <panayot.panayotov@siteground.com>
2020-06-26 10:12:16 +0300

    Encapsulate the logic in a class and add the functionality to save the response from alexa

--- dataprep/alexa_scrapper.py
+++ dataprep/alexa_scrapper.py
@@ -1,44 +1,53 @@
 import argparse
 import json
+import os
 
 import requests
 from bs4 import BeautifulSoup
 
-
-def get_score(element):
-
-    referral_sites = element.select("#card_mini_audience")[0]
-
-    score = [score.text for score in referral_sites.find_all('span', {'class', 'truncation'})]
-    urls = referral_sites.find_all('div', {'class': 'site'})[1:]
-
-    alexa_ranks = [el.span.text for el in element.find_all('div', {'class': 'metric_two'})[1:6]]
-
-    result = []
-    for url, score, alexa_rank in zip(urls, score, alexa_ranks):
-        url = url.text.strip('\n').strip('\t').strip(' ').strip('\t').strip('\n').strip(' ').strip('\t').strip('\n')
-        score = float(score.replace(',', ''))
-        alexa_rank = float(alexa_rank[:-2].replace(',', ''))
-        result.append({'url': url, 'score': score, 'alexa_rank': alexa_rank})
-
-    return result
+_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 
 
-def scrape_alexa_site_info(target_site):
-    response = requests.get(f"https://www.alexa.com/siteinfo/{target_site}")
-    element = BeautifulSoup(response.text, 'lxml')
+class ScrapeAlexa():
+    @staticmethod
+    def _get_alexa_audience_metrics(element):
+        """ Returns data about audience overlap like:
+        similar sites, overlap score, alexa rank
+        :param element: BeautifulSoup object
+        """
+        referral_sites = element.select("#card_mini_audience")[0]
 
-    countries = element.find_all('div', {'id': 'countryName'})
-    percentages = element.find_all('div', {'id': 'countryPercent'})
+        score = [score.text for score in referral_sites.find_all('span', {'class', 'truncation'})]
+        urls = referral_sites.find_all('div', {'class': 'site'})[1:]
 
-    res = {}
+        alexa_ranks = [el.span.text for el in element.find_all('div', {'class': 'metric_two'})[1:6]]
 
-    res['site'] = target_site
-    res['score'] = get_score(element)
-    res['audience_geography'] = [{'country': country.text.split("\xa0")[-1], 'percent': float(percent.text[:-1])}
-                                  for country, percent in zip(countries, percentages)]
+        result = []
+        for url, score, alexa_rank in zip(urls, score, alexa_ranks):
+            url = url.text.strip('\n').strip('\t').strip(' ').strip('\t').strip('\n').strip(' ').strip('\t').strip('\n')
+            score = float(score.replace(',', ''))
+            alexa_rank = float(alexa_rank[:-2].replace(',', ''))
+            result.append({'url': url, 'score': score, 'alexa_rank': alexa_rank})
 
-    return res
+        return result
+
+    def scrape_alexa_site_info(self, target_site):
+        response = requests.get(f"https://www.alexa.com/siteinfo/{target_site}")
+        with open(os.path.join(_DATA_PATH, f'{target_site}.html'), 'w') as f:
+            f.write(response.text)
+        element = BeautifulSoup(response.text, 'lxml')
+
+        countries = element.find_all('div', {'id': 'countryName'})
+        percentages = element.find_all('div', {'id': 'countryPercent'})
+
+        res = {}
+
+        res['site'] = target_site
+        res['score'] = ScrapeAlexa._get_alexa_audience_metrics(element)
+        res['audience_geography'] = [{'country': country.text.split("\xa0")[-1], 'percent': float(percent.text[:-1])}
+                                      for country, percent in zip(countries, percentages)]
+
+        return res
 
 
 if __name__ == '__main__':
@@ -47,10 +56,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    result = scrape_alexa_site_info(args.target_site)
-    print(result)
+    result = ScrapeAlexa().scrape_alexa_site_info(args.target_site)
 
-    with open(f'{args.target_site}.json', 'w') as f:
+    with open(os.path.join(_DATA_PATH, f'{args.target_site}.json'), 'w') as f:
         json.dump(result, f, indent=4)
 
 """
