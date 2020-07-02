2e3f6ed972d8bcbc08ebd20cad1eec860c0df0b4
PacoPacov <panayot.panayotov@siteground.com>
2020-07-02 17:46:57 +0300

    Update the parsing of the html page and add checks for reaching request limit

--- dataprep/alexa_scrapper.py
+++ dataprep/alexa_scrapper.py
@@ -11,11 +11,13 @@ _LOGGER = logging.getLogger('dataprep.alexa_scrapper')
 
 
 class ScrapeAlexa:
-    def __init__(self, target_site):
+    def __init__(self, target_site, target_dir=None):
         self.target_site = target_site
+        self.target_dir = target_dir if target_dir else _DATA_PATH
+        os.makedirs(self.target_dir, exist_ok=True)
 
     def _is_site_already_checked(self):
-        return f'{self.target_site}.html' in os.listdir(_DATA_PATH)
+        return f'{self.target_site}.html' in os.listdir(self.target_dir)
 
     @staticmethod
     def _get_alexa_audience_metrics(element):
@@ -24,36 +26,47 @@ class ScrapeAlexa:
         :param element: BeautifulSoup object
         """
 
-        similar_sites_by_audience_overlap = element.select("#card_mini_audience")
-        if not similar_sites_by_audience_overlap:
-            return []
+        similar_sites_by_audience_overlap = element.find('div', {'id': 'card_mini_audience'})
+        audience_overlap = element.find('div', {'id': 'card_overlap'})
 
-        referral_sites = similar_sites_by_audience_overlap[0]
+        if similar_sites_by_audience_overlap:
+            resources = similar_sites_by_audience_overlap
+        elif audience_overlap:
+            resources = audience_overlap
+        else:
+            return
 
-        score = [score.text for score in referral_sites.find_all('span', {'class', 'truncation'})]
-        urls = referral_sites.find_all('div', {'class': 'site'})[1:]
+        similar_sites = [el['href'] for el in resources.find_all('a', {'class': 'truncation'})]
+        overlap_score = [el.text for el in resources.find_all('span', {'class': 'truncation'})]
 
-        alexa_ranks = [el.span.text for el in element.find_all('div', {'class': 'metric_two'})[1:6]]
+        sites_overlap_score = overlap_score[::2]
+        alexa_score = overlap_score[1::2]
 
         # TODO Add implementation on how to handle when where isn't 'referral_sites'
-        result = []
-        for url, score, alexa_rank in zip(urls, score, alexa_ranks):
-            url = url.text.strip('\n').strip('\t').strip(' ').strip('\t').strip('\n').strip(' ').strip('\t').strip('\n')
-            score = float(score.replace(',', '')) if score.strip() != '-' else None
-            alexa_rank = float(alexa_rank[:-2].replace(',', '')) if alexa_rank.strip() != '-' else None
-            result.append({'url': url, 'score': score, 'alexa_rank': alexa_rank})
 
-        return result
+        return [{'url': site.replace("/siteinfo/", ''),
+                 'overlap_score': float(ov_score) if ov_score.strip() != '-' else None,
+                 'alexa_rank': float(al_score.replace(',', '')) if al_score.strip() != '-' else None}
+                for site, ov_score, al_score in zip(similar_sites, sites_overlap_score, alexa_score)]
 
     def scrape_alexa_site_info(self):
         if self._is_site_already_checked():
             _LOGGER.info(f"This site '{self.target_site}' has already being processed")
-            return
+            with open(f"{self.target_dir}/{self.target_site}.html") as f:
+                response = f.read()
 
-        response = requests.get(f"https://www.alexa.com/siteinfo/{self.target_site}")
-        with open(os.path.join(_DATA_PATH, f'{self.target_site}.html'), 'w') as f:
-            f.write(response.text)
-        element = BeautifulSoup(response.text, 'lxml')
+            element = BeautifulSoup(response, 'lxml')
+
+        else:
+            time.sleep(10)
+            response = requests.get(f"https://www.alexa.com/siteinfo/{self.target_site}")
+
+            if 'Forbidden' in response.text:
+                raise ValueError("You've exceeded the request limit for the day!")
+
+            with open(os.path.join(self.target_dir, f'{self.target_site}.html'), 'w') as f:
+                f.write(response.text)
+            element = BeautifulSoup(response.text, 'lxml')
 
         countries = element.find_all('div', {'id': 'countryName'})
         percentages = element.find_all('div', {'id': 'countryPercent'})
@@ -61,9 +74,15 @@ class ScrapeAlexa:
         res = {}
 
         res['site'] = self.target_site
-        res['score'] = ScrapeAlexa._get_alexa_audience_metrics(element)
-        res['audience_geography'] = [{'country': country.text.split("\xa0")[-1], 'percent': float(percent.text[:-1])}
-                                      for country, percent in zip(countries, percentages)]
+        score = ScrapeAlexa._get_alexa_audience_metrics(element)
+        if not score:
+            _LOGGER.info(f"No similar_sites_by_audience_overlap or audience_overlap found for {self.target_site}")
+        res['score'] = score
+        if countries:
+            res['audience_geography'] = [{'country': country.text.split("\xa0")[-1], 'percent': float(percent.text[:-1])}
+                                          for country, percent in zip(countries, percentages)]
+        else:
+            res['audience_geography'] = []
 
         return res
 
