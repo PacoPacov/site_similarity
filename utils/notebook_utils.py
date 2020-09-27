c60d87ac3bf1b10bb177b476198654a430ac6df0
unknown <p.d.panayotov09@gmail.com>
2020-09-27 04:37:43 +0300

    notebook_utils.py: Add new way of evaluating clf models and train node2vec models

--- utils/notebook_utils.py
+++ utils/notebook_utils.py
@@ -1,21 +1,38 @@
-import csv
 import json
 import os
 
-from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-from stellargraph import StellarGraph, data
-import networkx as nx
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+import networkx as nx
+import numpy as np
+import pandas as pd
+import redis
+from bs4 import BeautifulSoup
+from gensim.models import Word2Vec
+from sklearn import svm
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix, f1_score, mean_absolute_error
+from stellargraph import StellarGraph
+from stellargraph.data import BiasedRandomWalk
 
-from dataprep.load_annotated_data import _ANNOTATED_DATA_PATH
+from dataprep.alexa_scrapper import ScrapeAlexa
+from dataprep.load_annotated_data import load_corpus, load_splits
 
-from dataprep.load_annotated_data import apply_splits, load_corpus, load_splits
-_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
-_INVALID_URLS_2018 = os.path.join(_ANNOTATED_DATA_PATH, 'invalid_urls_2018.json')
-_ALL_DATA = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dataset', 'all_data')
+_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
+_ALL_DATA = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'dataset', 'all_data')
+_MODEL_STORAGE = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'node2vec_models')
+
+
+def evaluate(y_test, y_test_pred):
+    return {
+        "Accuracy": accuracy_score(y_test, y_test_pred),
+        "Balanced Accuracy score": balanced_accuracy_score(y_test, y_test_pred),
+        "F1 micro score": f1_score(y_test, y_test_pred, average='micro'),
+        "F1 macro score": f1_score(y_test, y_test_pred, average='macro'),
+        "F1 weighted score": f1_score(y_test, y_test_pred, average='weighted'),
+        "MAE": mean_absolute_error(y_test, y_test_pred),
+        "Confusion matrix": confusion_matrix(y_test, y_test_pred).tolist()
+    }
 
 
 def calculate_metrics(actual, predicted):
@@ -41,39 +58,19 @@ def calculate_metrics(actual, predicted):
 
     return f1, accuracy, flip_err, mae
 
-def train_model(clf, node2vec_model, data_year='2020', num_labels=3):
+
+def train_model(clf, data_year='2020', node2vec_model=None, num_labels=3):
     label2int = {
         "fact": {"low": 0, "mixed": 1, "high": 2},
         "bias": {"left": 0, "center": 1, "right": 2},
     }
 
-    # int2label = {
-    #     "fact": {0: "low", 1: "mixed", 2: "high"},
-    #     "bias": {0: "left", 1: "center", 2: "right"},
-    # }
-
     if data_year == '2020':
-        DATA = load_corpus('new_corpus_2020.csv')
-        SPLITS = load_splits('modified_splits_new_corpus_2020.json')
+        DATA = load_corpus('new_corpus_2020.csv', data_year='2020')
+        SPLITS = load_splits('modified_splits_new_corpus_2020.json', data_year='2020')
     elif data_year == '2018':
-        DATA = load_corpus('modified_corpus_2018.csv')
-        LOADED_SPLITS = load_splits('splits_2018.json')
-
-        with open(_INVALID_URLS_2018, 'r') as f:
-            invalid_urls_2018 = json.load(f)
-
-        SPLITS = []
-        for index in range(len(LOADED_SPLITS)):
-
-            train_index_urls = [invalid_urls_2018[train_url] if train_url in invalid_urls_2018 else train_url
-                                for train_url in LOADED_SPLITS[index][f"train-{index}"].split('\n')]
-
-            test_index_urls = [invalid_urls_2018[test_url] if test_url in invalid_urls_2018 else test_url
-                               for test_url in LOADED_SPLITS[index][f"test-{index}"].split('\n')]
-
-            SPLITS.append({f'test-{index}': '\n'.join(test_index_urls),
-                           f'train-{index}': '\n'.join(train_index_urls)})
-
+        DATA = load_corpus('corpus_2018_20200907.tsv', data_year='2018', delimiter='\t')
+        SPLITS = load_splits('modified_split_2018_20200907.json', data_year='2018')
     else:
         raise ValueError(f'Incorrect parameter "data_year" = {data_year}')
 
@@ -81,24 +78,21 @@ def train_model(clf, node2vec_model, data_year='2020', num_labels=3):
 
     df['source_url_processed'] = df['source_url_processed'].apply(lambda x: 'hemaven.net' if x == 'themaven.net' else x)
     num_folds = len(SPLITS)
-    print(f"Splits: {type(SPLITS)} len: {len(SPLITS)}")
 
     # create a dictionary: the keys are the media and the values are their corresponding labels (transformed to int)
-    labels = {df["source_url_processed"].str.lower()[i]: label2int['fact'][df['fact'].str.lower()[i]] for i in range(df.shape[0])}
+    labels = {df["source_url_processed"].str.lower()[i]: label2int['fact'][df['fact'].str.lower()[i]]
+              for i in range(df.shape[0])}
 
     # create placeholders where predictions will be cumulated over the different folds
     all_urls = []
     actual = np.zeros(len(df), dtype=np.int)
     predicted = np.zeros(len(df), dtype=np.int)
-    probs = np.zeros((len(df), num_labels), dtype=np.float)
 
     i = 0
 
     print("Start training...")
 
     for index in range(num_folds):
-        print(f"Fold: {index}")
-
         # get the training and testing media for the current fold
         urls = {
             "train": SPLITS[index][f"train-{index}"].split('\n'),
@@ -111,12 +105,16 @@ def train_model(clf, node2vec_model, data_year='2020', num_labels=3):
         X, y = {}, {}
 
         # concatenate the different features/labels for the training sources
-        X["train"] = np.asmatrix([node2vec_model.wv[url] for url in urls["train"]]).astype("float")
-        y["train"] = np.array([labels[url] for url in urls["train"]], dtype=np.int)
+        if node2vec_model:
+            X["train"] = np.asmatrix([node2vec_model.wv[url] for url in urls["train"]]).astype("float")
+            X["test"] = np.asmatrix([node2vec_model.wv[url] for url in set(urls["test"])]).astype("float")
+        else:
+            X["train"] = np.array([url for url in urls["train"]])
+            X["test"] = np.array([url for url in set(urls["test"])])
 
+        y["train"] = np.array([labels[url] for url in urls["train"]], dtype=np.int)
         # concatenate the different features/labels for the testing sources
-        X["test"] = np.asmatrix([node2vec_model.wv[url] for url in urls["test"]]).astype("float")
-        y["test"] = np.array([labels[url] for url in urls["test"]], dtype=np.int)
+        y["test"] = np.array([labels[url] for url in set(urls["test"])], dtype=np.int)
 
         # train the classifier
         clf.fit(X["train"], y["train"])
@@ -124,45 +122,14 @@ def train_model(clf, node2vec_model, data_year='2020', num_labels=3):
         # generate predictions
         pred = clf.predict(X["test"])
 
-        # generate probabilites
-        prob = clf.predict_proba(X["test"])
+        # cumulate the actual and predicted labels, and the probabilities over the different folds. then, move the index
+        actual[i: i + len(y["test"])] = y["test"]
 
-        # cumulate the actual and predicted labels, and the probabilities over the different folds.  then, move the index
-        actual[i: i + y["test"].shape[0]] = y["test"]
-        predicted[i: i + y["test"].shape[0]] = pred
-        probs[i: i + y["test"].shape[0], :] = prob
+        predicted[i: i + len(y["test"])] = pred
         i += y["test"].shape[0]
 
     # calculate the performance metrics on the whole set of predictions (5 folds all together)
-    results = calculate_metrics(actual, predicted)
-
-    # display the performance metrics
-    print(f"Accuracy: {results[1]}")
-    print(f"Macro-F1: {results[0]}")
-    print(f"Flip Error-rate: {results[2]}")
-    print(f"MAE: {results[3]}")
-
-    # # map the actual and predicted labels to their categorical format
-    # predicted = np.array([int2label[args.task][int(l)] for l in predicted])
-    # actual = np.array([int2label[args.task][int(l)] for l in actual])
-
-    # # create a dictionary: the keys are the media, and the values are their actual and predicted labels
-    # predictions = {all_urls[i]: (actual[i], predicted[i]) for i in range(len(all_urls))}
-
-    # # create a dataframe that contains the list of m actual labels, the predictions with probabilities.  then store it in the output directory
-    # df_out = pd.DataFrame({"source_url": all_urls, "actual": actual, "predicted": predicted, int2label[args.task][0]: probs[:, 0], int2label[args.task][1]: probs[:, 1], int2label[args.task][2]: probs[:, 2],})
-    # columns = ["source_url", "actual", "predicted"] + [int2label[args.task][i] for i in range(args.num_labels)]
-    # df_out.to_csv(os.path.join(out_dir, "predictions.tsv"), index=False, columns=columns)
-
-    # # write the experiment results in a tabular format
-    # res = PrettyTable()
-    # res.field_names = ["Macro-F1", "Accuracy", "Flip error-rate", "MAE"]
-    # res.add_row(results)
-
-    # # write the experiment summary and outcome into a text file and save it to the output directory
-    # with open(os.path.join(out_dir, "results.txt"), "w") as f:
-    #     f.write(summary.get_string(title="Experiment Summary") + "\n")
-    #     f.write(res.get_string(title="Results"))
+    return evaluate(actual, predicted)
 
 
 def create_graph(lvl_data, root):
@@ -197,19 +164,58 @@ def load_level_data(data_path=None, level=0):
     return output
 
 
-def save_note2vec_model(model):
-    pass
+def create_node2vec_model(lvl_data, is_weighted, dimension=None, file_name=None, prefix=None, dimensions=[]):
+    """Creates a node2vec model and saves it.
+    :param lvl_data: data that will be used to create the model
+    :dimension: Integer value that tells in which dimension should the embeddings be
+    :is_weighted: Boolean value that indicates whenever the graph is weighted or not
+    :file_name: Name of the file where the model will be saved. Please use the file extention '.model'
+    """
+    # TODO Add more checks for the input parameters
+    if not dimensions:
+        dimensions = [dimension]
+
+    if not file_name:
+        weight = 'unweighted' if not is_weighted else 'weight'
+        file_names = [f"{prefix}_{weight}_{str(dimension)}_D.model" for dimension in dimensions]
+    else:
+        file_names = [file_name]
+
+    columns = ['source', 'target', 'weight'] if is_weighted else ['source', 'target']
+
+    lvl_one_graph = StellarGraph(edges=pd.DataFrame(lvl_data, columns=columns))
+
+    rw = BiasedRandomWalk(lvl_one_graph)
+
+    print("Start creating random walks")
+    walks = rw.run(
+        nodes=list(lvl_one_graph.nodes()),  # root nodes
+        length=100,  # maximum length of a random walk
+        n=10,  # number of random walks per root node
+        p=0.5,  # Defines (unormalised) probability, 1/p, of returning to source node
+        q=2.0,  # Defines (unormalised) probability, 1/q, for moving away from source node
+        weighted=is_weighted,  # for weighted random walks
+        seed=42,  # random seed fixed for reproducibility
+    )
+    print("Number of random walks: {}".format(len(walks)))
+
+    str_walks = [[str(n) for n in walk] for walk in walks]
+
+    for d, model_name in zip(dimensions, file_names):
+        model = Word2Vec(str_walks, size=d, window=5, min_count=0, sg=1, workers=2, iter=1)
+
+        os.makedirs(_MODEL_STORAGE, exist_ok=True)
+
+        if model_name in os.listdir(_MODEL_STORAGE):
+            raise ValueError(f'Model {model_name} already exists in {_MODEL_STORAGE}!')
+
+        model.save(os.path.join(_MODEL_STORAGE, model_name))
+
+        print(f"Successful save of model: {model_name}!")
 
 
-def load_corpus(corpus_file):
-    corpus = []
-    with open(os.path.join(_ANNOTATED_DATA_PATH, corpus_file)) as f:
-        csv_reader = csv.DictReader(f)
-
-        for row in csv_reader:
-            corpus.append(row)
-
-    return corpus
+def load_note2vec_model(model_name):
+    return Word2Vec.load(os.path.join(_MODEL_STORAGE, model_name))
 
 
 def create_nodes(lvl_data):
@@ -235,27 +241,20 @@ def create_weighted_nodes(lvl_data):
     return nodes
 
 
-def get_referral_sites(site, all_data_dir):
-    file_name = os.path.join(all_data_dir, f"{site}.html")
+def get_referral_sites(site):
+    redis_client = redis.Redis()
 
-    if not os.path.exists(file_name):
-        return
+    if redis_client.exists(site):
+        return json.loads(redis_client.get(site))
 
-    with open(file_name) as f:
-        text = f.read()
+    else:
+        alexa_scrapper = ScrapeAlexa()
+        content = alexa_scrapper.get_site_content(site)
+        referral_sites = alexa_scrapper._get_alexa_referral_sites_metric(content)
 
-    text = BeautifulSoup(text, 'html')
+        redis_client.set(site, json.dumps(referral_sites))
 
-    referral_sites = text.find('div', {'id': 'card_referralsites'})
-
-    if not referral_sites:
-        return []
-
-    found_sites = referral_sites.find_all('div', {'class': 'Row'})
-
-    return [(found_site.find('div', {'class', 'site'}).a['href'].split('/')[-1],
-            found_site.find('span', {'class': 'truncation'}).text.strip())
-            for found_site in found_sites]
+        return referral_sites
 
 
 def get_site_metrics(site, all_data_dir=_ALL_DATA):
@@ -265,9 +264,9 @@ def get_site_metrics(site, all_data_dir=_ALL_DATA):
         return
 
     with open(file_name) as f:
-        text = f.read()
+        html_text = f.read()
 
-    text = BeautifulSoup(text, 'html')
+    text = BeautifulSoup(html_text, 'html')
 
     # Site Metrics
     card_metrics = text.find('div', {'id': 'card_metrics'})
@@ -281,7 +280,7 @@ def get_site_metrics(site, all_data_dir=_ALL_DATA):
     result = {}
     fields = ['Daily Pageviews per Visitor', 'Daily Time on Site', 'Bounce rate']
     for field, stat in zip(fields, stats):
-        stat = stat.text.replace("\n\t\t\t\t\t\t\t\t\t          \t            ", '').split(' ')[0]
+        stat = stat.text.strip(' \t\n').split(' ')[0]
         result[field] = stat
 
     # Alexa rank
@@ -291,10 +290,50 @@ def get_site_metrics(site, all_data_dir=_ALL_DATA):
         pass
 
     alexa_rank = {}
-    alexa_rank['alexa_rank'] = card_rank.find('p', {'class': 'big data'}).text.replace('\n\t\t\t\t\t\t\t\t\t          \t        ', '')
-    alexa_rank['start_rank'] = card_rank.find('div', {'class', 'start-rank'}).text#.find('span', {'class', 'rank'}).text
-    alexa_rank['end_rank'] = card_rank.find('div', {'class', 'end-rank'}).text#.find('span', {'class', 'rank'}).text
+    alexa_rank['alexa_rank'] = card_rank.find('p', {'class': 'big data'}).text.strip(' \t\n')
+    alexa_rank['alexa_rank_in_past_three_months'] = text.find('script', {'id': 'rankData'}).string
+    alexa_rank['total_sites_linked_in'] = card_rank.find('span', {'class': 'big data'}).text
 
     print(alexa_rank)
 
     return result
+
+
+def eval_node2vec_models(models, data_year):
+    result_report = []
+
+    for model in models:
+        print(f'Using model: {model}')
+        node2vec_model = load_note2vec_model(model)
+
+        clf = LogisticRegressionCV(Cs=10, cv=5, scoring="accuracy", multi_class="ovr", max_iter=300, random_state=42)
+        result_report.append([
+            model.strip('.model'),
+            'LogisticRegression CV = 5',
+            *list(train_model(clf, node2vec_model=node2vec_model, data_year=data_year).values())
+        ])
+
+        clf2 = LogisticRegressionCV(Cs=10, cv=10, scoring="accuracy", multi_class="ovr", max_iter=300, random_state=42)
+        result_report.append([
+            model.strip('.model'),
+            'LogisticRegression CV = 10',
+            *list(train_model(clf2, node2vec_model=node2vec_model, data_year=data_year).values())
+        ])
+
+        tree_clf = GradientBoostingClassifier(random_state=42)
+        result_report.append([
+            model.strip('.model'),
+            'GradientBoostingClassifier',
+            *list(train_model(tree_clf, node2vec_model=node2vec_model, data_year=data_year).values())
+        ])
+
+        svm_clf = svm.SVC(decision_function_shape='ovo', probability=True, random_state=42)
+        result_report.append([
+            model.strip('.model'),
+            'SVC ovo',
+            *list(train_model(svm_clf, node2vec_model=node2vec_model, data_year=data_year).values())
+        ])
+
+    return pd.DataFrame(result_report,
+                        columns=["Feature", "Classifier", "Accuracy", "Balanced Accuracy score",
+                                 "F1 micro score", "F1 macro score", "F1 weighted score", "MAE", "Confusion matrix"])
